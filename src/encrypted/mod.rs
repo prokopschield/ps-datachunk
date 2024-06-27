c27f5dd9fb83bd7567d515f3ac3625b0097ab3f6
Prokop <prokop.schield@protonmail.com>
2024-06-27 14:25:19 +0200

    chore(src/encrypted): implement DataChunkTrait for EncryptedDataChunk

--- src/encrypted/mod.rs
+++ src/encrypted/mod.rs
@@ -1,3 +1,4 @@
+use crate::DataChunkTrait;
 use crate::OwnedDataChunk;
 use crate::PsDataChunkError;
 use ps_cypher::Compressor;
@@ -13,5 +14,17 @@ impl EncryptedDataChunk {
     /// Decrypts this `EncryptedDataChunk`.
     pub fn decrypt(&self, compressor: &Compressor) -> Result<OwnedDataChunk, PsDataChunkError> {
         OwnedDataChunk::decrypt(&self.chunk, self.key.as_bytes(), compressor)
+    }
+}
+
+impl DataChunkTrait for EncryptedDataChunk {
+    fn data_ref(&self) -> &[u8] {
+        self.chunk.data_ref()
+    }
+    fn hash_ref(&self) -> &[u8] {
+        self.chunk.hash_ref()
+    }
+    fn hash(&self) -> crate::HashCow {
+        self.chunk.hash()
     }
 }
