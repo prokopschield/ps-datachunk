3dd3cd17056c0ce510a53bda0c7a3ee608cf767e
Prokop <prokop.schield@protonmail.com>
2025-04-20 17:40:49 +0200

    chore: remove unused constants

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -2,14 +2,8 @@ use ps_hash::Hash;
 
 use super::rounding::round_up;
 
-/// 16 bytes
-pub const HASH_ALIGNMENT: usize = 4;
-
 /// 8 bytes
 pub const SIZE_ALIGNMENT: usize = 3;
 
 /// 64 bytes
 pub const HASH_SIZE: usize = round_up(std::mem::size_of::<Hash>(), SIZE_ALIGNMENT);
-
-/// 8 bytes on x86-64
-pub const SIZE_SIZE: usize = round_up(std::mem::size_of::<usize>(), SIZE_ALIGNMENT);
