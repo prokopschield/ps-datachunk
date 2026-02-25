4a09b6ebebc741f9ea8dbf3b7bebdb011ce08fd0
Prokop Schield <prokop.schield@protonmail.com>
2026-02-25 14:04:57 +0100

    refactor: remove duplicate and unused error variants

--- src/error.rs
+++ src/error.rs
@@ -20,22 +20,14 @@ pub enum PsDataChunkError {
     PsCypherError(#[from] ps_cypher::PsCypherError),
     #[error(transparent)]
     TryFromSliceError(#[from] TryFromSliceError),
-    #[error("Failed to serialize into an AlignedDataChunk")]
-    SerializationError,
     #[error("The data chunk was not correctly layed out")]
     InvalidDataChunk,
     #[error("The hash of a chunk was incorrect")]
     InvalidHash,
-    #[error("Invalid length: {0}")]
-    InvalidLength(usize),
     #[error("Rkyv deserialization failed")]
     RkyvInvalidArchive,
     #[error("Rkyv serialization failed")]
     RkyvSerializationFailed,
-    #[error("This should never happen: {0}")]
-    ShouldNotHaveFailed(&'static str),
-    #[error("DataChunk content does not match the type it is being interpreted as")]
-    TypeError,
 }
 
 pub type Result<T> = std::result::Result<T, PsDataChunkError>;
